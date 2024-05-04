aaefc4505500dc52e183395db73348d9f173406d
yhyoon1004 <yhyoon1004@naver.com>
2024-05-04 11:49:39 +0900

    Initial commit

---
+++ src/test/java/spring_advance/advance/AdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_advance.advance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
