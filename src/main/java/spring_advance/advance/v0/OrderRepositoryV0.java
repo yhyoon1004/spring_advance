794cc88b2d1c8c1818639da758bb79d225a99740
yhyoon1004 <yhyoon1004@naver.com>
2024-05-05 14:17:01 +0900

    예제 프로젝트 만들기 - V0

---
+++ src/main/java/spring_advance/advance/v0/OrderRepositoryV0.java
@@ -0,0 +1,27 @@
+package spring_advance.advance.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생!");
+        }
+        sleep(1000);
+    }
+
+    private static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
