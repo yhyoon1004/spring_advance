d8f19995880f325c1841bbdb589e5a3a880be352
yhyoon1004 <yhyoon1004@naver.com>
2024-05-17 22:33:58 +0900

    템플릿 메서드 패턴 - 예제3

--- src/test/java/spring_advance/advance/trace/template/TemplateMethodTest.java
+++ src/test/java/spring_advance/advance/trace/template/TemplateMethodTest.java
@@ -42,4 +42,22 @@ public class TemplateMethodTest {
         abstractTemplate2.execute();
     }
 
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        };
+        template1.execute();
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        };
+        template2.execute();
+    }
+
 }
