2910ef5885f60736870a83731999d306b6441536
yhyoon1004 <yhyoon1004@naver.com>
2024-05-19 14:44:16 +0900

    전략 패턴 - 시작

---
+++ src/test/java/spring_advance/advance/trace/strategy/ContextV1Test.java
@@ -0,0 +1,31 @@
+package spring_advance.advance.trace.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+
+    @Test
+    public void strategyV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        log.info("비지니스 로직1 실행");
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        log.info("비지니스 로직2 실행");
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+}
