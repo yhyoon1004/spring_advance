2b11a4361570d8f73d58d02cd35080d750321af9
yhyoon1004 <yhyoon1004@naver.com>
2024-05-21 19:59:35 +0900

    전략 패턴 - 예제2

--- src/test/java/spring_advance/advance/trace/strategy/ContextV1Test.java
+++ src/test/java/spring_advance/advance/trace/strategy/ContextV1Test.java
@@ -3,6 +3,7 @@ package spring_advance.advance.trace.strategy;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import spring_advance.advance.trace.strategy.code.strategy.ContextV1;
+import spring_advance.advance.trace.strategy.code.strategy.Strategy;
 import spring_advance.advance.trace.strategy.code.strategy.StrategyLogic1;
 import spring_advance.advance.trace.strategy.code.strategy.StrategyLogic2;
 
@@ -30,6 +31,10 @@ public class ContextV1Test {
         long resultTime = endTime - startTime;
         log.info("resultTime = {}", resultTime);
     }
+
+    /**
+     * 전략 패턴 사용
+     * */
     @Test
     void strategyV1() {
         StrategyLogic1 strategyLogic1 = new StrategyLogic1();
@@ -39,4 +44,61 @@ public class ContextV1Test {
         ContextV1 contextV2 = new ContextV1(strategyLogic2);
         contextV2.execute();
     }
+
+
+    @Test
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        };
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        log.info("strategyLogic1={}",strategyLogic1.getClass());
+        context1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        };
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        log.info("strategyLogic2={}",strategyLogic2.getClass());
+        context2.execute();
+    }
+
+    @Test
+    void strategyV3 () {
+        ContextV1 context1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        });
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        });
+        context2.execute();
+    }
+    @Test
+    void strategyV4 () {
+
+        ContextV1 contextXXX = new ContextV1(() -> {
+            log.info("비지니스 로직1 실행");
+        });
+        //구현해야하는 인터페이스의 메서드가 1개일 경우 람다 치환 가능
+        ContextV1 context1 = new ContextV1(() -> log.info("비지니스 로직1 실행"));
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(() -> log.info("비지니스 로직1 실행"));
+        context2.execute();
+    }
+
 }
